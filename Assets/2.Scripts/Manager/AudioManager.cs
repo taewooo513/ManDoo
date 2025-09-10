1f6ca7c214634de01b3a650d9cde4fe8e1a2b445
taewooo513 <taewooo030513@gmail.com>
2025-09-10 16:08:59 +0900

    [feat] 사운드매니저 오브젝트매니저 리소스매니저 추가

--- Assets/2.Scripts/Manager/AudioManager.cs
+++ Assets/2.Scripts/Manager/AudioManager.cs
@@ -1,18 +1,56 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    Dictionary<string, AudioClip> sounds;
+    AsyncOperationHandle soundHandle;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        sounds = new Dictionary<string, AudioClip>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InsertSound(string key, AudioClip audioClip)
     {
-        
+        if (sounds.TryGetValue(key, out AudioClip clip))
+        {
+            Debug.Log($"{key} is duplicate in sounds");
+            return;
+        }
+        sounds.Add(key, clip);
+    }
+
+    public AsyncOperationHandle LoadSound(string label)
+    {
+        var handle = ResourceManager.Instance.LoadResource<AudioClip>(label, clip =>
+        {
+            sounds.Add(clip.name, clip);
+        });
+        handle.Completed += OnLoadCompleteObject;
+        soundHandle = handle;
+        return soundHandle;
+    }
+    private void OnLoadCompleteObject<T>(AsyncOperationHandle<IList<T>> handle) where T : UnityEngine.Object
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            Debug.Log("Load Sounds Succeeded");
+        }
+        else if (handle.Status == AsyncOperationStatus.Failed)
+        {
+            Debug.LogError("Load Sounds Failed");
+        }
+    }
+
+    public override void Release()
+    {
+        sounds.Clear();
+        Addressables.Release(soundHandle);
     }
 }
