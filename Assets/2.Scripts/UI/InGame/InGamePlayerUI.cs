e0f89c5d44a7499531153a4555abcffa5f6f3870
taewooo513 <taewooo030513@gmail.com>
2025-09-15 14:15:57 +0900

    Merge to Develop

---
+++ Assets/2.Scripts/UI/InGame/InGamePlayerUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InGamePlayerUI : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI jobText;
+
+    public TextMeshProUGUI hpUI;
+    public TextMeshProUGUI currentHpUI;
+    
+    
+}
