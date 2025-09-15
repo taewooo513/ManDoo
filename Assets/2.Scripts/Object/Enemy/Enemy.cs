6c88c9b6c62247f0e92d0be6721644dc66194089
Insung Park <sleepwithoutadream@gmail.com>
2025-09-15 15:54:46 +0900

    [Feat] Enemy 와 Skill 스크립트 작성

--- Assets/2.Scripts/Object/Enemy/Enemy.cs
+++ Assets/2.Scripts/Object/Enemy/Enemy.cs
@@ -1,30 +1,46 @@
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+using DataTable;
 
-public class Enemy : MonoBehaviour
+public class Enemy : BaseEntity
 {
-    //private Skill skill;
-    //List<Skill> skillList = new List<Skill>();
+    List<Skill> skillList = new List<Skill>();
     private float percentage;
-    private float standardPercentage;
+    private float standardPercentage = 0.25f;
+    private int mark = -1;
+    private int _id;
+    private EnemyData ed;
 
-    // private void Awake()
-    // {
-    //     skill = GetComponent<Skill>();
-    // }
+    public void Init(int id)
+    {
+        _id = id;
+        ed = DataManager.Instance.Enemy.GetEnemyData(_id);
+        SetSkill();
+    }
 
-    // public Skill GetRandomSkill()
-    // {
+    private void SetSkill()
+    {
+        foreach (var id in ed.skillId)
+        {
+            Skill skill = new Skill();
+            skill.Init(id);
+            skillList.Add(skill);
+        }
+    }
 
-    // }
+    public Skill GetRandomSkill()
+    {
+        int rand = UnityEngine.Random.Range(1, 4);
+        return skillList[rand];
+    }
 
-    public void OnClickSelectEnemy()
+    public override void Attack(int index)
     {
         
     }
 
-    public void Attack()
+    public void OnClickSelectEnemy()
     {
 
     }
@@ -41,7 +57,7 @@ public class Enemy : MonoBehaviour
 
     public void Buff()
     {
-
+        
     }
 
     public void Guard()
@@ -54,3 +70,37 @@ public class Enemy : MonoBehaviour
 
     }
 }
+
+/*
+
+    public bool GetDroppedItem(int dropTable, out GameObject droppedItems)
+    {
+        float total = 0f;
+        foreach (var drop in DropItemTables[dropTable].DropItemDatas)
+        {
+            total += drop.Percent;
+        }
+
+        float random = UnityEngine.Random.value * total;
+        int id = 0;
+        foreach (var drop in DropItemTables[dropTable].DropItemDatas)
+        {
+            random -= drop.Percent;
+            if (random <= 0f)
+            {
+                id = drop.ID;
+                break;
+            }
+        }
+
+        if (id == 0)
+        {
+            droppedItems = null;
+            return false;
+        }
+
+        droppedItems = ItemDatas[id].Prefab;
+        return true;
+    }
+
+*/
