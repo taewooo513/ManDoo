41f5602c2d68ece079ef75557e105349daf12d75
권진석 <rnjswlstjr1@naver.com>
2025-09-15 14:40:13 +0900

    [fix] 플레이어

--- Assets/2.Scripts/Object/Player/Player.cs
+++ Assets/2.Scripts/Object/Player/Player.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Playerablecharter : BaseEntity
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Attack(int index)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Attack(index);
     }
 }
