f94682357e23e06698dea3c1257c71139852df40
권진석 <rnjswlstjr1@naver.com>
2025-09-15 15:26:22 +0900

    [feat]플레이어

--- Assets/2.Scripts/Object/Player/Player.cs
+++ Assets/2.Scripts/Object/Player/Player.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using DataTable;
+using DefaultTable;
 using UnityEngine;
 
-public class Playerablecharter : BaseEntity
+public class PlayableCharacter : BaseEntity
 {
+    private int id;
+    private MercenaryData data;
+
+    private void SetData()
+    {
+        data = DataManager.Instance.Mercenary.GetMercenaryData(id);
+    }
+
+    public void init(int idx)
+    {
+        id = idx;
+        SetData();
+    }
     public override void Attack(int index)
     {
         base.Attack(index);
