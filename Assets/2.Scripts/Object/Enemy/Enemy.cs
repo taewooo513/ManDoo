99632be9bf7c3cea518d61b93fb30c92ece3b163
taewooo513 <taewooo030513@gmail.com>
2025-09-16 10:43:56 +0900

    [ref] baseEntity 공격, 스킬함수인자값 인덱스 -> BaseEntity 전환

--- Assets/2.Scripts/Object/Enemy/Enemy.cs
+++ Assets/2.Scripts/Object/Enemy/Enemy.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 using DataTable;
 using System;
@@ -53,9 +53,9 @@ public class Enemy : BaseEntity
         return possibleSkills[UnityEngine.Random.Range(0, possibleSkills.Count)];
     }
 
-    public override void Attack(int index)
+    public override void Attack(BaseEntity baseEntity)
     {
-        base.Attack(index);
+        base.Attack(baseEntity);
     }
 
     private void AttackPercentage()
