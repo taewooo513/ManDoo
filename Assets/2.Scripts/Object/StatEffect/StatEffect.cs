1eb1048bf24b909b20acea21eff37cb7a0eb63c0
myeoungeun <melee0110@naver.com>
2025-09-16 18:05:33 +0900

    [feat] 상태이상 부여, 가중치 부여 개발 완료

--- Assets/2.Scripts/Object/StatEffect/StatEffect.cs
+++ Assets/2.Scripts/Object/StatEffect/StatEffect.cs
@@ -6,11 +6,11 @@ public class StatEffect
 {
     private List<StatEffectInfo> _entityCurrentStatus = new();
     private SetTotalEffectStat _setTotalEffectStat;
+    private float totalWeight = 0f;
 
-    private void AttackPercentage() //가중치 부여
-    {
-        float total = 10;
-        float rand = UnityEngine.Random.value * total;
+    public void AttackPercentage() //가중치 계산/부여
+    { //스킬 쓸 때마다 호출
+        totalWeight = 0;
         
         //상태이상 효과에 따라 다른 가중치 부여
         for (int i = 0; i < _entityCurrentStatus.Count;)
@@ -19,26 +19,53 @@ public class StatEffect
             {
                 var status = _entityCurrentStatus[i].entityStatus[j];
                 if (status == StatusType.Normal) return;
-                if (status == StatusType.Mark) { }
-                if (status == StatusType.Buff) { }
-                if (status == StatusType.Debuff) { }
-                if (status == StatusType.Guard) { }
-                if (status == StatusType.Guardian) { }
-                if (status == StatusType.PlayerReactAtk) { }
-                if (status == StatusType.PlayerReactSupport) { }
+                if (status == StatusType.Mark)
+                {
+                    totalWeight += SetTotalEffectStat.Mark;
+                }
+
+                if (status == StatusType.Buff)
+                {
+                    totalWeight += SetTotalEffectStat.Buff;
+                }
+
+                if (status == StatusType.Debuff)
+                {
+                    totalWeight += SetTotalEffectStat.Debuff;
+                }
+
+                if (status == StatusType.Guard)
+                {
+                    totalWeight += SetTotalEffectStat.Guard;
+                }
+
+                if (status == StatusType.Guardian)
+                {
+                    totalWeight += SetTotalEffectStat.Guardian;
+                }
+
+                if (status == StatusType.PlayerReactAtk)
+                {
+                    totalWeight += SetTotalEffectStat.PlayerReactAtk;
+                }
+
+                if (status == StatusType.PlayerReactSupport)
+                {
+                    totalWeight += SetTotalEffectStat.PlayerReactSupport;
+                }
             }
 
-            // if () //버프 지속시간이 끝나면
-            // {
-            //     RemoveStatus(_entityCurrentStatus[i]);
-            // }
-            // else
-            // {
-            //     i++;
-            // }
+            if (_entityCurrentStatus[i].duration <= 0) //버프 지속시간이 끝나면
+            {
+                RemoveStatus(_entityCurrentStatus[i]); //상태이상 제거
+            }
+            else
+            {
+                _entityCurrentStatus[i].duration--; //턴 감소
+                i++;
+            }
         }
     }
-
     
     public void AddStatus(StatEffectInfo status) //상태이상 추가
     {
@@ -53,26 +80,5 @@ public class StatEffect
     public void Clear() //리스트 내부의 배열 삭제(상태이상 초기화)
     {
       _entityCurrentStatus.Clear();
-    }
-
-    
-    //상태이상 효과들
-    public void WeightCalculation()
-    {
-        for (int i = 0; i < _entityCurrentStatus.Count;)
-        {
-            for (int j = 0; j < _entityCurrentStatus[i].entityStatus.Count; j++)
-            {
-                var status = _entityCurrentStatus[i].entityStatus[j];
-                if (status == StatusType.Normal) return;
-                if (status == StatusType.Mark) { }
-                if (status == StatusType.Buff) { }
-                if (status == StatusType.Debuff) { }
-                if (status == StatusType.Guard) { }
-                if (status == StatusType.Guardian) { }
-                if (status == StatusType.PlayerReactAtk) { }
-                if (status == StatusType.PlayerReactSupport) { }
-            }
-        }
     }
 }
--- Assets/2.Scripts/Object/StatEffect/StatEffectInfo.cs
+++ Assets/2.Scripts/Object/StatEffect/StatEffectInfo.cs
@@ -10,4 +10,10 @@ public enum StatusType
 public class StatEffectInfo //상태이상 효과 정보 리스트
 {
     public List<StatusType> entityStatus = new();
+    public int duration;
+
+    public void Init(SkillInfo skill) //스킬 사용할 때 호출
+    {
+        duration = skill.duration;
+    }
 }
