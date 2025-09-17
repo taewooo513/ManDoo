30657aca814ae6378f9b1268383cff64037dd9c1
taewooo513 <taewooo030513@gmail.com>
2025-09-17 14:52:34 +0900

    [feat] 스킬 사용함수 구현

---
+++ Assets/2.Scripts/Object/Skill/Skill.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataTable;
+using System.Linq;
+
+public class SkillInfo
+{
+    public string skillName;
+    public EffectType effectType;
+    public float adRatio;
+    public int constantValue;
+    public int duration;
+    public TargetType targetType;
+    public List<int> enablePos;
+    public List<int> targetPos;
+    public string iconPathString;
+
+    public SkillInfo(string skillName, EffectType effectType, float adRatio
+        , int constantValue, int duration, TargetType targetType, List<int> enablePos, List<int> targetPos,
+        string iconPathString)
+    {
+        this.skillName = skillName;
+        this.effectType = effectType;
+        this.adRatio = adRatio;
+        this.constantValue = constantValue;
+        this.duration = duration;
+        this.targetType = targetType;
+        this.enablePos = enablePos;
+        this.targetPos = targetPos;
+        this.iconPathString = iconPathString;
+    }
+}
+
+public class Skill
+{
+    private SkillData sd;
+    public SkillInfo skillInfo { get; private set; }
+
+    SkillEffect[] skillEffects;
+
+    public void Init(int id)
+    {
+        sd = DataManager.Instance.Skill.GetSkillData(id);
+        skillInfo = new SkillInfo(
+            sd.skillName, sd.effectType, sd.adRatio, sd.constantValue,
+            sd.duration, sd.targetType, sd.enablePos, sd.targetPos, sd.iconPathString);
+    }
+
+    public void Setting()
+    {
+
+    }
+
+    public void UseSkill(float ad, BaseEntity targetEntity)
+    {
+        for (int i = 0; i < skillEffects.Length; i++)
+        {
+        }
+    }
+}
