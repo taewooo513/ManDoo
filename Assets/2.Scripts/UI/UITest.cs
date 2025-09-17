abb7021e30055c3efd47f7ceac330f4253555293
권진석 <rnjswlstjr1@naver.com>
2025-09-17 20:01:19 +0900

    [fix]UI테스트

--- Assets/2.Scripts/UI/UITest.cs
+++ Assets/2.Scripts/UI/UITest.cs
@@ -7,6 +7,7 @@ public class UITest : MonoBehaviour
     
     void Start()
     {
+        DataManager.Instance.Initialize();
         UIManager.Instance.OpenUI<InGameUIManager>();
         UIManager.Instance.OpenUI<InGamePlayerUI>();
         UIManager.Instance.OpenUI<InGameInventoryUI>();
