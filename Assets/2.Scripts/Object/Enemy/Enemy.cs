3565e966b6dca861ce73aeb671364f4d379a1a81
Insung Park <sleepwithoutadream@gmail.com>
2025-09-18 16:09:32 +0900

    [feat] Enemy.cs -> 턴종료 추가

--- Assets/2.Scripts/Object/Enemy/Enemy.cs
+++ Assets/2.Scripts/Object/Enemy/Enemy.cs
@@ -117,14 +117,16 @@ public class Enemy : BaseEntity
 
         if (CanUseSkill(attackSkill))
         {
-            if(targetRange.Contains(pickedIndex)) //선택한 인덱스(때리려는 적)가 타겟 가능한 위치에 있는지 체크
+            if (targetRange.Contains(pickedIndex)) //선택한 인덱스(때리려는 적)가 타겟 가능한 위치에 있는지 체크
             {
                 attackSkill.UseSkill(BattleManager.Instance.PlayableCharacters[pickedIndex]); //스킬 사용
+                EndTurn();
             }
             else //없으면 위치 바꾸기
             {
                 int position = GetDesiredPosition(attackSkill); //현재 enemy가 서 있는 위치
                 BattleManager.Instance.SwitchPosition(this, position);
+                EndTurn();
             }
         }
     }
