89f9e051d10d349534d9751714b78c932380d8b4
권진석 <rnjswlstjr1@naver.com>
2025-09-19 16:16:23 +0900

    [feat]스킬셀렉

--- Assets/2.Scripts/UI/InGame/SelectEntityButton/SelectSkillButton.cs
+++ Assets/2.Scripts/UI/InGame/SelectEntityButton/SelectSkillButton.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SelectSkillButton : MonoBehaviour
+public class SelectSkillButton : MonoBehaviour/*, IPointerEnterHandler, IPointerExitHandler*/
 {
 
     private Button skillButton;
     private InGameUIManager inGameUIManager;
 
+    //public GameObject[] targetImage;
+
     private void Awake()
     {
         skillButton = GetComponent<Button>();
         inGameUIManager = UIManager.Instance.OpenUI<InGameUIManager>();
+        //if (targetImage != null)
+        //{
+        //    foreach (var img in targetImage)
+        //        if (img != null) img.SetActive(false);
+        //}
     }
     public void SetButton(Skill skill)
     {
@@ -23,4 +31,21 @@ public class SelectSkillButton : MonoBehaviour
     {
         inGameUIManager.OnClickSkillButton(skill);
     }
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+    //    if (targetImage != null)
+    //    {
+    //        foreach (var img in targetImage)
+    //            if (img != null) img.SetActive(true);
+    //    }
+    //}
+
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    if (targetImage != null)
+    //    {
+    //        foreach (var img in targetImage)
+    //            if (img != null) img.SetActive(false);
+    //    }
+    //}
 }
