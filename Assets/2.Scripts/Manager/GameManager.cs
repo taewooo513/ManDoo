3d46af2b36256b42f103e396870fba59435286dd
taewooo513 <taewooo030513@gmail.com>
2025-09-19 14:37:20 +0900

    [test] 게임 시작 테스트용

--- Assets/2.Scripts/Manager/GameManager.cs
+++ Assets/2.Scripts/Manager/GameManager.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     private List<BaseEntity> playableCharacter;
+    private List<BaseEntity> enemyCharacter;
 
     private void Awake()
     {
         playableCharacter = new List<BaseEntity>();
+        enemyCharacter = new List<BaseEntity>();
     }
 
-    public void SelectPlayer(BaseEntity baseEntity)
+    public void AddPlayer(BaseEntity baseEntity)
     {
         playableCharacter.Add(baseEntity);
+    }
+
+    public void AddEnemy(BaseEntity baseEntity)
+    {
+        enemyCharacter.Add(baseEntity);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartBattle();
+        }
     }
 
     public void StartGame()
@@ -24,8 +39,7 @@ public class GameManager : Singleton<GameManager>
     public void StartBattle()
     {
         List<BaseEntity> baseEntities = new List<BaseEntity>();
-        //BattleManager.Instance.AddEnemyCharacter();
-        BattleManager.Instance.BattleStartTrigger(playableCharacter, baseEntities);
+        BattleManager.Instance.BattleStartTrigger(playableCharacter, enemyCharacter);
     }
 
     public void EndGame()
