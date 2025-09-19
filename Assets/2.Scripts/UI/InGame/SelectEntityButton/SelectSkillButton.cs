e4d340fd5f2bffa4718a72338b3058839a127f08
권진석 <rnjswlstjr1@naver.com>
2025-09-19 16:59:01 +0900

    [feat]적표시스크립트

--- Assets/2.Scripts/UI/InGame/SelectEntityButton/SelectSkillButton.cs
+++ Assets/2.Scripts/UI/InGame/SelectEntityButton/SelectSkillButton.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SelectSkillButton : MonoBehaviour/*, IPointerEnterHandler, IPointerExitHandler*/
+public class SelectSkillButton : MonoBehaviour
 {
 
     private Button skillButton;
     private InGameUIManager inGameUIManager;
 
-    //public GameObject[] targetImage;
-
     private void Awake()
     {
         skillButton = GetComponent<Button>();
         inGameUIManager = UIManager.Instance.OpenUI<InGameUIManager>();
-        //if (targetImage != null)
-        //{
-        //    foreach (var img in targetImage)
-        //        if (img != null) img.SetActive(false);
-        //}
+        
     }
     public void SetButton(Skill skill)
     {
@@ -31,21 +25,5 @@ public class SelectSkillButton : MonoBehaviour/*, IPointerEnterHandler, IPointer
     {
         inGameUIManager.OnClickSkillButton(skill);
     }
-    //public void OnPointerEnter(PointerEventData eventData)
-    //{
-    //    if (targetImage != null)
-    //    {
-    //        foreach (var img in targetImage)
-    //            if (img != null) img.SetActive(true);
-    //    }
-    //}
-
-    //public void OnPointerExit(PointerEventData eventData)
-    //{
-    //    if (targetImage != null)
-    //    {
-    //        foreach (var img in targetImage)
-    //            if (img != null) img.SetActive(false);
-    //    }
-    //}
+    
 }
