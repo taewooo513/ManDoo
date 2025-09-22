687db4609c9d3ea04bd67a63a4a864b526855b54
권진석 <rnjswlstjr1@naver.com>
2025-09-22 17:42:38 +0900

    [feat]용병샵UI

---
+++ Assets/2.Scripts/UI/InGame/PMCUI/PMCInfoUI.cs
@@ -0,0 +1,41 @@
+﻿using DataTable;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PMCInfo : BaseEntity
+{
+    private MercenaryData data;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI roleTypeText;
+    public TextMeshProUGUI Money;
+
+    [SerializeField] private int initID;
+
+
+    public void Start()
+    {
+        SetData(initID);
+    }
+
+    private void SetData(int id)
+    {
+        this.id = id;
+        data = DataManager.Instance.Mercenary.GetMercenaryData(id);
+
+        entityInfo = new EntityInfo(
+            data.name, data.health, data.attack, data.defense, data.speed, data.evasion, data.critical
+        );
+        entityInfo.SetUpSkill(data.skillId, this);
+
+        // UI 표시 추가!
+        if (nameText != null)
+            nameText.text = data.name;
+        if (roleTypeText != null)
+            roleTypeText.text = data.roleType.ToString();
+        //if (Weapon != null)
+        //    Weapon.text = data.weaponLv; // MercenaryData에 무기레벨이 생기면
+    }
+}
