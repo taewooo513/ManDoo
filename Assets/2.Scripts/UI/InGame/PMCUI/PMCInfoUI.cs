4ae3426e71e5415a2c349ca6d53447ee6a073dce
권진석 <rnjswlstjr1@naver.com>
2025-09-22 19:12:58 +0900

    [fix]PMCUI

--- Assets/2.Scripts/UI/InGame/PMCUI/PMCInfoUI.cs
+++ Assets/2.Scripts/UI/InGame/PMCUI/PMCInfoUI.cs
@@ -14,7 +14,6 @@ public class PMCInfo : BaseEntity
 
     [SerializeField] private int initID;
 
-
     public void Start()
     {
         SetData(initID);
@@ -28,14 +27,12 @@ public class PMCInfo : BaseEntity
         entityInfo = new EntityInfo(
             data.name, data.health, data.attack, data.defense, data.speed, data.evasion, data.critical
         );
-        entityInfo.SetUpSkill(data.skillId, this);
 
         // UI 표시 추가!
         if (nameText != null)
             nameText.text = data.name;
         if (roleTypeText != null)
             roleTypeText.text = data.roleType.ToString();
-        //if (Weapon != null)
-        //    Weapon.text = data.weaponLv; // MercenaryData에 무기레벨이 생기면
+        
     }
 }
