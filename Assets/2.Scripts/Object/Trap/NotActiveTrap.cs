affca96f9a32394be68407123e82a576a5c55ee7
myeoungeun <melee0110@naver.com>
2025-09-22 19:41:07 +0900

    [feat] 함정 버튼

--- Assets/2.Scripts/Object/Trap/NotActiveTrap.cs
+++ Assets/2.Scripts/Object/Trap/NotActiveTrap.cs
@@ -1,7 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NotActiveTrap : MonoBehaviour
 {
+    public GameObject outline;
+
+    public void Start()
+    {
+        outline = gameObject.GetComponent<GameObject>();
+    }
 }
