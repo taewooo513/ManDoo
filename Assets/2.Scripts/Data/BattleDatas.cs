2003833ed83893db77b0d955bfede19b4842bfde
taewooo513 <taewooo030513@gmail.com>
2025-09-23 17:14:35 +0900

    [feat] 소모품, 전투씬데이터 생성

---
+++ Assets/2.Scripts/Data/BattleDatas.cs
@@ -0,0 +1,12 @@
+﻿using DataTable;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleDatas : BattleData
+{
+    public BattleData GetSkillData(int idx)
+    {
+        return BattleDataMap[idx];
+    }
+}
