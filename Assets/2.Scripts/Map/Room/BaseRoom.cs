c2e6b9d20d2197160a02756c5f785b68aa3f9942
myeoungeun <melee0110@naver.com>
2025-09-23 14:50:41 +0900

    [chore] 위치 정리

--- Assets/2.Scripts/Map/Room/BaseRoom.cs
+++ Assets/2.Scripts/Map/Room/BaseRoom.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class BaseRoom : MonoBehaviour
 {
-    void Start()
+    public Dictionary<int, GameObject> playableCharacterDic; //int에 키값, 게임오브젝트에 대응하는 프리팹
+    //데이터테이블에 있는 id 값 넣고, 리소스에 있는 프리팹을 게임오브젝트에 넣어주는 작업 필요함
+    
+    public virtual void EnterRoom()
     {
+        // int[] id = { 0 }; //현재 용병 리스트가 계속 바뀔 수 있으니, 방에 들어갈 때마다
+        // for (int i = 0; i < BattleManager.Instance.PlayableCharacters.Count; i++) //현재 리스트가 가진 id 값들 체크 
+        // {
+        //     id[i]  = BattleManager.Instance.PlayableCharacters[i].id;
+        // }
         
+        //id[i]가 프리팹과 같은 id를 가졌으면
+        //특정 위치에 플레이어 소환하기
     }
 
-    void Update()
+    public virtual void ExitRoom()
     {
-        
     }
 }
--- Assets/2.Scripts/Map/Room/VillageRoom.cs
+++ Assets/2.Scripts/Map/Room/VillageRoom.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleRoom : BaseRoom
+public class VillageRoom : BaseRoom //마을, 보스방
 {
     public override void EnterRoom()
     {
@@ -10,5 +10,8 @@ public class BattleRoom : BaseRoom
 
     public override void ExitRoom()
     {
+        //탐색한 방의 수
+        //파티 초상화
+        //플레이 보상
     }
 }
