d14c190a5da2b378f829c2665633341a1245120b
ChanyLee <chanyblackbear@gmail.com>
2025-09-24 11:05:50 +0900

    [feat] BaseRoom에 RoomLocation String 추가

--- Assets/2.Scripts/Map/Room/BattleRoom.cs
+++ Assets/2.Scripts/Map/Room/BattleRoom.cs
@@ -6,10 +6,15 @@ public class BattleRoom : BaseRoom
 {
     public Spawn spawn;
 
-    public override void EnterRoom() //방 입장 시
+    public BattleRoom()
     {
         GameObject spawnObject = new GameObject("Spawn");
-        Spawn spawn = spawnObject.AddComponent<Spawn>();
+        spawn = spawnObject.AddComponent<Spawn>();
+        Debug.Log(spawn);
+    }
+
+    public override void EnterRoom() //방 입장 시
+    {
         var battleData = DataManager.Instance.Battle.GetBattleData(1001); //TODO : 1001 수정해야됨 //배틀데이터 dt에서 id 챙겨옴
         spawn.EnemySpawn(battleData.battleEnemies); //적 소환
     }
--- Assets/2.Scripts/Object/Spawn.cs
+++ Assets/2.Scripts/Object/Spawn.cs
@@ -12,8 +12,12 @@ public class Spawn : MonoBehaviour
 
     public void EnemySpawn(List<int> id)
     {
-        Vector3 pos = new Vector3(-1, 0, 0);
-        Vector3 add = new Vector3(2, 0, 0);
+        foreach (var item in id)
+        {
+            Debug.Log(item);
+        }
+        Vector3 pos = new Vector3(1, -1, 0);
+        Vector3 add = new Vector3(0, 2, 0);
         
         for (int i = 0; i < id.Count; i++) //적 특정 위치에 생성
         {
