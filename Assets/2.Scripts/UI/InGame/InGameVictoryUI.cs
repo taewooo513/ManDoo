dcd93af6f81041bab8a550184b5c004960b42730
myeoungeun <melee0110@naver.com>
2025-09-25 10:52:52 +0900

    [feat] 승리 ui 제작

--- Assets/2.Scripts/UI/InGame/InGameVictoryUI.cs
+++ Assets/2.Scripts/UI/InGame/InGameVictoryUI.cs
@@ -1,8 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InGameVictoryUI : InGameBattleStartUI
+public class InGameVictoryUI : UIBase
 {
+    public Button yesButton;
+    public Button noButton;
+
+    public void Start()
+    {
+        Button yesBtn = yesButton.GetComponent<Button>();
+        Button noBtn = noButton.GetComponent<Button>();
+        yesBtn.onClick.AddListener(YesButtonOnClick);
+        noBtn.onClick.AddListener(NoButtonOnClick);
+    }
+
+    public void YesButtonOnClick()
+    {
+        Debug.Log("You have clicked yes button!");
+    }
     
+    public void NoButtonOnClick()
+    {
+        Debug.Log("You have clicked no button!");
+        UIManager.Instance.CloseUI<InGameVictoryUI>();
+    }
 }
