1da921a94d53b7833585521426acca662700441b
myeoungeun <melee0110@naver.com>
2025-09-25 09:43:05 +0900

    [comment] 테스트코드

--- Assets/2.Scripts/Others/Test.cs
+++ Assets/2.Scripts/Others/Test.cs
@@ -17,5 +17,6 @@ public class Test : MonoBehaviour
         startRoom.EnterRoom(1001);
         
         //UIManager.Instance.OpenUI<InGameBattleStartUI>(); //ui 테스트
+        GameManager.Instance.StartBattle();
     }
 }
