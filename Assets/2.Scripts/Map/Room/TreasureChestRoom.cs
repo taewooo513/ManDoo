30bbea87f6549ecb7d8240f933a9eded8d55d0eb
권진석 <rnjswlstjr1@naver.com>
2025-09-25 21:28:19 +0900

    [feat]상점

--- Assets/2.Scripts/Map/Room/TreasureChestRoom.cs
+++ Assets/2.Scripts/Map/Room/TreasureChestRoom.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreasureChestRoom : MonoBehaviour
 {
+    [SerializeField] private Button treasureChestButton;
+
     private void Start()
     {
         UIManager.Instance.OpenUI<InGameTreasureChestUI>();
+        treasureChestButton.onClick.AddListener(OnClickTreasureChest);
     }
 
     private void OnClickTreasureChest()
     {
-        Debug.Log("보물상자 클릭됨");   
+        Debug.Log("보물상자 클릭됨");
+        UIManager.Instance.OpenUI<InGameVictoryUI>();
     }
 }
