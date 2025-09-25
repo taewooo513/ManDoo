e0bdd6af1e090ac3a3db6a3426acf77738e8637e
권진석 <rnjswlstjr1@naver.com>
2025-09-25 11:21:48 +0900

    [feat]보물상자

---
+++ Assets/2.Scripts/Map/Room/TreasureChestRoom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TreasureChestRoom : MonoBehaviour
+{
+    private void Start()
+    {
+        UIManager.Instance.OpenUI<InGameTreasureChestUI>();
+    }
+
+    private void OnClickTreasureChest()
+    {
+        Debug.Log("보물상자 클릭됨");   
+    }
+}
