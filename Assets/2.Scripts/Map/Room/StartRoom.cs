ab913f7e727e3157f4714aa63bf8422a4b28be07
myeoungeun <melee0110@naver.com>
2025-09-27 00:35:15 +0900

    [fix] 스타트룸 돌아왔을 때 플레이어 재생성되는 오류

--- Assets/2.Scripts/Map/Room/StartRoom.cs
+++ Assets/2.Scripts/Map/Room/StartRoom.cs
@@ -12,7 +12,11 @@ public class StartRoom : BaseRoom
     public override void EnterRoom()
     {
         base.EnterRoom(); //스폰 챙겨오기
-        spawn.PlayableCharacterCreate(1004); //시작 플레이어(1004) 생성
+        if (!isInteract) //첫 1회만 생성
+        {
+            spawn.PlayableCharacterCreate(1004); //시작 플레이어(1004) 생성
+            isInteract = true;
+        }
         spawn.PlayableCharacterSpawn(); //플레이어 소환(위치 선정)
 
         InventoryManager.Instance.TryAddItem(eItemType.Consumable,1001, 1500); //1500 골드 지급
