d2ea3b21b6343b5c7620623e06c60c867eef74ed
myeoungeun <melee0110@naver.com>
2025-09-26 10:53:16 +0900

    [feat] 배틀룸 구현완료

--- Assets/2.Scripts/Map/Room/TreasureRoom.cs
+++ Assets/2.Scripts/Map/Room/TreasureRoom.cs
@@ -13,10 +13,10 @@ public class TreasureRoom : BaseRoom
     {
         base.ExitRoom(id);
         
-        //ItemManager.Instance.AddItem(1001, randomGoldDropCount); //랜덤 개수대로 골드 아이템 추가
+        ItemManager.Instance.AddReward(eItemType.Consumable,1001, randomGoldDropCount); //랜덤 개수대로 골드 아이템 추가
         if (randomPercentage < dropProb) //드랍 확률대로 아이템 떨어짐 (ex : 0.25% 확률로 아이템 떨어짐)
         {
-            //ItemManager.Instance.AddItem(dropItem, 1); //아이템 추가 todo : 예시 코드라서 다른곳의 add함수 필요함. 추가하고 ui쪽에서 지우는것도 필요함
+            ItemManager.Instance.AddReward(eItemType.Consumable, dropItem, 1); //아이템 추가
         }
     }
 }
